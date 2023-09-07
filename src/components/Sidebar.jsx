19e396bcc6c5ae69853b4812146da203f7f5e333
Declan Brown <135834907+DeclanKBrown@users.noreply.github.com>
2023-09-07 13:02:01 +1000

    Added sidebar top buttons

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+import '../styles/sidebar.css'
+import SidebarTop from './SidebarTop'
+
+export default function Sidebar() {
+    return (
+        <div className='sidebar'>
+            <SidebarTop />
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Page from './components/Page'
+import Sidebar from './components/Sidebar'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Sidebar />
     <Page />
   </React.StrictMode>,
 )
