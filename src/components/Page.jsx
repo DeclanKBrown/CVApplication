1a78255cbaeeea8b24da9840f21b7ef745d69a2b
Declan Brown <135834907+DeclanKBrown@users.noreply.github.com>
2023-09-07 12:01:19 +1000

    Restarting from page

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -4,6 +4,9 @@ export default function Page() {
     return (
         <>
           <div className="a4">
+            <div className='personal-info'>
+              <h1></h1>
+            </div>
 
           </div>
         </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Sidebar from './components/Sidebar'
 import Page from './components/Page'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Sidebar />
     <Page />
   </React.StrictMode>,
 )
